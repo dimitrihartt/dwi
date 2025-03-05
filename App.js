1ee63def01a8c515e68629c996e0e00e4e2c33bf
Dimitri Hartt <idevelop4jesus@gmail.com@git config --global user.email idevelop4jesus@gmail.com>
2025-03-05 20:48:55 -0300

    DoneWithIt App

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import AccountScreen from "./app/screens/AccountScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <MessagesScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
